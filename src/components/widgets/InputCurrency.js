edf730ed2f959982f794566e8b647673400546c2
mustermaxs <max.sinnl@hotmail.com>
2023-03-10 17:02:55 +0100

    fixed currency symbol positioning issue

--- src/components/widgets/InputCurrency.js
+++ src/components/widgets/InputCurrency.js
@@ -5,27 +5,29 @@ import "./input.css";
 function InputCurrency(props) {
   return (
     <>
-      {props.label == undefined ? (
+      {props.label === undefined ? (
         ""
       ) : (
         <label className="bb-input-label" for={props.id}>
           {props.label}
         </label>
       )}
-      <div className="currencySymbol">
-        {props.currency == undefined ? "€" : props.currency}
+      <div>
+        <input
+          placeholder={props.placeholder}
+          id={props.id}
+          name={props.name}
+          className={
+            "bb-input currency " +
+            (props.size === undefined ? "large" : props.size)
+          }
+          type="number"
+          step="0.01"
+        ></input>
+        <div className="currencySymbol">
+          {props.currency === undefined ? "€" : props.currency}
+        </div>
       </div>
-      <input
-        placeholder={props.placeholder}
-        id={props.id}
-        name={props.name}
-        className={
-          "bb-input currency " +
-          (props.size == undefined ? "large" : props.size)
-        }
-        type="number"
-        step="0.01"
-      ></input>
     </>
   );
 }
