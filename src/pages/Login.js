b9c4297b0f71c5c17d740f9f38778b0928903de6
mustermaxs <max.sinnl@hotmail.com>
2023-03-20 20:37:19 +0100

    added state to input, onchange, onclick handlers

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,28 @@
 import ReactDOM from "react-dom/client";
+import { useState } from "react";
 import BbBtn from "../components/widgets/BbBtn";
 import ContentWrapper from "../components/widgets/ContentWrapper";
 import InputText from "../components/widgets/InputText";
+import { Link } from "react-router-dom";
 
 function Login() {
+  const [inputValues, setValue] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleSubmit = () => {
+    // TODO
+    console.log(inputValues);
+  };
+
+  const handleChange = (name, newValue) => {
+    setValue((prevValue) => ({
+      ...prevValue,
+      [name]: newValue,
+    }));
+  };
+
   return (
     <>
       <ContentWrapper>
@@ -12,6 +31,7 @@ function Login() {
           name="username"
           label="Username"
           placeholder="Username"
+          onChange={(value) => handleChange("username", value)}
         />
 
         <InputText
@@ -19,9 +39,13 @@ function Login() {
           name="password"
           label="Password"
           placeholder="Password"
+          onChange={(value) => handleChange("password", value)}
         />
       </ContentWrapper>
-      <BbBtn content="Next" />
+      <BbBtn content="Login" type="button" onClick={handleSubmit} />
+      <Link className="bb-link-small" to="/registration">
+        Don't have an account yet? <br></br> Register here
+      </Link>
     </>
   );
 }
