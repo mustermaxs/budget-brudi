e04ff18702b4159517ee0f80a2458b04dbbfec66
mustermaxs <max.sinnl@hotmail.com>
2023-03-21 18:53:42 +0100

    fixed icon styling issue

--- src/components/widgets/Card.js
+++ src/components/widgets/Card.js
@@ -1,33 +1,49 @@
-import React, { useState } from 'react';
-import "./Card.css"
+import React, { useState } from "react";
+import "./Card.css";
+import getRandomInt from "../../utils/Random";
 // import images from "../../assets/icons/food.png";
 
-const Card = ({ icon, title, date, tags, price }) => {
-    const [tagState, setTagState] = useState(tags);
+const Card = (props) => {
+  const [tagState, setTagState] = useState(props.tags);
 
-    const image = require(`../../assets/icons/${icon}.png`);
+  // TODO import colorlabels from external resource?
+  //* MOCK
+  const colorLabels = ["#EAC435", "#345995", "#07A0C3", "#FB4D3D", "#CA1551"];
+  const randomColor = colorLabels[getRandomInt(0, colorLabels.length - 1)];
+  const iconStyle = (() => {
+    if (props.type === "category")
+      return {
+        backgroundImage: `url(${require(`../../assets/icons/icons_raw/${props.icon}.png`)})`,
+      };
+    else if (props.type === "goals") return { background: randomColor };
+  })();
 
-    return (
-        <div className="card">
-            <div className="icon-container">
-                <img src={image} alt="icon" />
-            </div>
-            <div className="text-container">
-                <h2 className="title">{title}</h2>
-                <p className="date">{date}</p>
-                <div className="tags">
-                    {tagState.map(tag => (
-                        <div className="tag" key={`${title}-${tag}`}>
-                            {tag}
-                        </div>
-                    ))}
-                </div>
-            </div>
-            <div className="price-container">
-                <p className={`price ${price > 0 ? "green" : "red"}`}>€ {price}</p>
-            </div>
-        </div>
-    );
+  console.log(iconStyle);
+  return (
+    <div className="card">
+      <div className="icon-container" style={iconStyle}>
+        {/* <img src={image} alt="icon" /> */}
+      </div>
+      <div className="text-container">
+        <h2 className="title">{props.title}</h2>
+        <p className="date">{props.date}</p>
+        {props.tags !== undefined && (
+          <div className="tags">
+            {tagState.map((tag) => (
+              <div className="tag" key={`${props.title}-${tag}`}>
+                {tag}
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+      <div className="price-container">
+        <p className={`price ${props.price > 0 ? "green" : "red"}`}>
+          € {props.price}
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
--- src/pages/Transactions.js
+++ src/pages/Transactions.js
@@ -3,59 +3,58 @@ import ContentWrapper from "../components/widgets/ContentWrapper";
 import Card from "../components/widgets/Card";
 import Filter from "../components/widgets/Filter";
 
-
 function Transactions() {
+  const [cards, setCards] = useState([]);
 
-    const [cards, setCards] = useState([]);
+  const categories = ["work", "transportation", "health"];
 
-    const categories = ["work", "transportation", "health"]
+  const mockData = [
+    {
+      category: "work",
+      title: "Work Expense",
+      date: "04.03.2023",
+      tags: ["#Groceries", "#Birthday"],
+      price: 24.9,
+    },
+    {
+      category: "transportation",
+      title: "Tesla Abo",
+      date: "04.03.2023",
+      tags: ["#Work", "#Birthday"],
+      price: -24.9,
+    },
+    {
+      category: "health",
+      title: "Apotheke",
+      date: "04.03.2023",
+      tags: ["#Groceries"],
+      price: 24.9,
+    },
+  ];
 
-    const mockData = [
-        {
-            category: "work",
-            title: "Work Expense",
-            date: "04.03.2023",
-            tags: ["#Groceries", "#Birthday"],
-            price: 24.90,
-        },
-        {
-            category: "transportation",
-            title: "Tesla Abo",
-            date: "04.03.2023",
-            tags: ["#Work", "#Birthday"],
-            price: -24.90,
-        },
-        {
-            category: "health",
-            title: "Apotheke",
-            date: "04.03.2023",
-            tags: ["#Groceries"],
-            price: 24.90,
-        },
-    ]
+  const [category, setCategory] = useState();
 
-    const [category, setCategory] = useState();
+  return (
+    <>
+      <ContentWrapper>
+        <Filter category={category} categories={categories} />
 
-    return (
-        <>
-            <ContentWrapper>
-
-                <Filter category={category} categories={categories} />
-
-                {mockData.map(({ category, title, date, tags, price }) => {
-                    return (< Card
-                        icon={category}
-                        title={title}
-                        date={date}
-                        tags={tags}
-                        price={price}
-                        key={title}
-                    />)
-                })}
-
-            </ContentWrapper>
-        </>
-    );
+        {mockData.map(({ category, title, date, tags, price }) => {
+          return (
+            <Card
+              type="category"
+              icon={category}
+              title={title}
+              date={date}
+              tags={tags}
+              price={price}
+              key={title}
+            />
+          );
+        })}
+      </ContentWrapper>
+    </>
+  );
 }
 
 export default Transactions;
