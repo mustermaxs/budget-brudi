eb3a507087dc0aba27c45d9fe073810c954746be
mustermaxs <max.sinnl@hotmail.com>
2023-03-22 10:18:42 +0100

    if not input el, accepts data-value attribute

--- src/hooks/useValue.js
+++ src/hooks/useValue.js
@@ -4,9 +4,13 @@ function useValue(initialValues) {
   const [inputValue, setValue] = useState(initialValues);
 
   const handleChange = (inputName, event) => {
+    const newValue =
+      event.target.value !== undefined
+        ? event.target.value
+        : event.target.dataset.value;
     setValue((prevValue) => ({
       ...prevValue,
-      [inputName]: event.target.value,
+      [inputName]: newValue,
     }));
   };
 
