6947607e22442185c2c26351d66559bb5941d4a9
Asu Mic <77020175+asu-mic@users.noreply.github.com>
2023-03-26 08:45:58 +0100

    Titel randomizer
    each Categories are give 7 items from which the title can chosen.

--- php/pseudoExpenseData.php
+++ php/pseudoExpenseData.php
@@ -18,12 +18,30 @@ if ($conn->connect_error) {
 $categories = array('Bills', 'Groceries', 'Food', 'Health', 'Household', 'Media & Electronics', 'Mortgage', 'Restaurants', 'Sport', 'Savings', 'Subscriptions', 'Transportation', 'Work');
 $categoryAmounts = array('Mortgage' => 1230, 'Savings' => 50, 'Bills' => 575, 'Subscriptions' => 45); // reaccuring amounts
 
+$categoriesData = array(
+  'Groceries'     => array('Billa', 'Billa Plus', 'Hofer', 'Penny', 'Spar', 'Lidl', 'Metro'),
+  'Food'          => array('Pizza', 'Burger', 'Tacos', 'Sushi', 'Pasta', 'Sandwich', 'Salad'),
+  'Health'        => array('Gym Membership', 'Doctor Visit', 'Vitamins', 'Massage', 'Apothecary', 'Dental Care', 'Acupuncture'),
+  'Household'     => array('Cleaning Supplies', 'Furniture', 'Home Repair', 'Appliances', 'Bedding', 'Garden Tools', 'Pet Food'),
+  'Media & Electronics' => array('Headphones', 'Smartphone', 'Laptop', 'TV', 'Camera', 'Video Games', 'Speakers'),
+  'Restaurants'   => array('Fast Food', 'Italian', 'Chinese', 'Mexican', 'Thai', 'Seafood', 'Steakhouse'),
+  'Sport'         => array('Sport Eybl', 'Basketball', 'Soccer', 'Swimming', 'SportXXL', 'FitInn', 'Boulder Bar'),
+  'Subscriptions' => array('Netflix', 'Spotify', 'Magazines', 'Gym', 'Online Courses', 'Meal Kit', 'Amazon Prime'),
+  'Transportation'=> array('Lime', 'Parking', 'Uber', 'Car Rental', 'Public Transportation', 'Bike Share', 'Toll'),
+);
 // Generate 1000 expense data points with dates from the last 30 days
 $expenses = array();
 for ($i = 0; $i < 1000; $i++) {
     $category = $categories[rand(0, count($categories) - 1)];
     $amount = isset($categoryAmounts[$category]) ? $categoryAmounts[$category] : number_format(mt_rand(10, 2000) / 100, 2);
     $date = date('Y-m-d', strtotime('-' . rand(1, 30) . ' days'));
+
+// randomly choose the Title from one of the category's array attributes. 
+    if (array_key_exists($category, $categoriesData)) 
+    {
+      $items = $categoriesData[$category];
+      $title = $items[rand(0, count($items) - 1)];
+    }
     $expenses[] = array('category' => $category, 'amount' => $amount, 'date' => $date, 'title' => $title);
 }
 
@@ -38,7 +56,7 @@ foreach ($expenses as $expense) {
     $category = $expense['category'];
     $amount = $expense['amount'];
     $date = $expense['date'];
-    $title = $expense['title']; // TODO - need title randomizer( sothat title make sense with category)
+    $title = $expense['title']; 
     $stmt->execute();
 }
 
@@ -49,38 +67,5 @@ echo $stmt->affected_rows . " rows inserted\n";
 $stmt->close();
 $conn->close();
 
-/*
-// Define the expense categories and specific amounts
-$categories = array('Bills', 'Groceries', 'Food', 'Health', 'Household', 'Media & Electronics', 'Mortgage', 'Restaurants', 'Sport', 'Savings', 'Subscriptions', 'Transportation', 'Work');
-$categoryAmounts = array('Mortgage' => 1230, 'Savings' => 50, 'Bills' => 575, 'Subscriptions' => 45); // recurring amounts
-
-// Create a MySQL connection
-$servername = "localhost";
-$username = "username";
-$password = "password";
-$dbname = "database_name";
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Generate 1000 expense data points with dates from the last 30 days
-$expenses = array();
-for ($i = 0; $i < 1000; $i++) {
-  $category = $categories[rand(0, count($categories) - 1)];
-  $amount = isset($categoryAmounts[$category]) ? $categoryAmounts[$category] : number_format(rand(10, 2000), 2);
-  $date = date('Y-m-d', strtotime('-'.rand(0, 30).' days'));
-  $expenses[] = array('category' => $category, 'amount' => $amount, 'date' => $date);
-}
-
-// Insert the expense data into the MySQL database
-$values = array();
-foreach ($expenses as $expense) {
-  $values[] = "('".$conn->real_escape_string($expense['category'])."', '".$conn->real_escape_string($expense['amount'])."', '".$conn->real_escape_string($expense['date'])."')";
-}
-$insertQuery = 'INSERT INTO expenses (category, amount, date) VALUES '.implode(',', $values);
-if ($conn->query($insertQuery) === TRUE) {
-  echo count($expenses).' rows inserted';
-} else {
-  echo 'Error: '.$conn->error;
-}
-$conn->close();
 ?>
 */
