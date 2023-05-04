289f8b3e8624739da298a92170ae7735086b4718
Maximilian Sinnl <max.sinnl@hotmail.com>
2023-05-04 13:52:10 +0200

    links on home screen

---
+++ src/components/LinkCard.js
@@ -0,0 +1,42 @@
+import { Link } from "react-router-dom";
+import "./LinkCard.css";
+
+function LinkCard({title, href, icon})
+{
+
+    const iconStyle = (() => {
+        const categoryMapping = {
+          "Profile": "hubi",
+          "Transaction": "cash-flow",
+          "Goals": "rocket",
+          "Balance": "research",
+          "Default": "Blank"
+        };
+    
+        var iconPath = categoryMapping[icon] || categoryMapping.Default;
+    
+        
+          return {
+            backgroundImage: `url(${require(`../assets/icons/icons_raw/${iconPath}.png`)})`,
+          };
+          })();
+
+    return (
+        <>
+        <Link className="blankLink" to={`/${href}`}>
+                  <div
+        className="card"
+        onClick={() => {}}
+      >
+        <div className="icon-container" style={iconStyle}>
+        </div>
+        <div className="text-container">
+          <h2 className="title">{title}</h2>
+        </div>
+      </div>
+      </Link>
+        </>
+    )
+}
+
+export default LinkCard;
---
+++ src/pages/Home.js
@@ -0,0 +1,19 @@
+// import "./Home.css";
+import LinkCard from "../components/LinkCard";
+import ContentWrapper from "../components/widgets/ContentWrapper";
+
+function Home(props)
+{
+    return (
+        <>
+        <ContentWrapper>
+            <LinkCard title="Profile" href="profile" icon="Profile"/>
+            <LinkCard title="Transactions" href="transactions" icon="Transaction"/>
+            <LinkCard title="Goals" href="goals" icon="Goals"/>
+            <LinkCard title="Balance" href="balance" icon="Balance"/>
+        </ContentWrapper>
+        </>
+    )
+}
+
+export default Home;
