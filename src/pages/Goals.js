73fbb9d1f471689cb05e481cd1128694ea1d277a
Markus Rösner <markus.roesner@gmx.net>
2023-05-13 12:26:40 +0200

    goal mock data if no goals

--- src/pages/Goals.js
+++ src/pages/Goals.js
@@ -19,6 +19,41 @@ function Goals() {
   const [chartDataColor, setChartDataColor] = useState([]);
 
 
+  const mockData = [
+    {
+      "Title": "Buy new car",
+      "GoalID": 2,
+      "F_accountID": 2,
+      "Date": "2024-01-01",
+      "Color": "#345995",
+      "Amount": "25000.00"
+    },
+    {
+      "Title": "Pay off credit card debt",
+      "GoalID": 4,
+      "F_accountID": 2,
+      "Date": "2023-06-30",
+      "Color": "#FB4D3D",
+      "Amount": "5000.00"
+    },
+    {
+      "Title": "Invest in stocks",
+      "GoalID": 6,
+      "F_accountID": 2,
+      "Date": "2023-09-01",
+      "Color": "#535353",
+      "Amount": "10000.00"
+    },
+    {
+      "Title": "Save for child education",
+      "GoalID": 8,
+      "F_accountID": 2,
+      "Date": "2025-01-01",
+      "Color": "#07a0c3",
+      "Amount": "50000.00"
+    }
+  ]
+
   useEffect(() => {
     loadingAnim.show();
     fetch("http://localhost/budget-brudi/api/goals", {
@@ -33,6 +68,9 @@ function Goals() {
         return res.json();
       })
       .then((goals) => {
+        if (goals.data.length === 0) {
+          goals.data = mockData;
+        }
 
         setChartLabels(goals.data.map(goal => goal.Title));
         setChartData(goals.data.map(goal => parseFloat(goal.Amount)));
