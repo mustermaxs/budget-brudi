73a236e25cda3a407ae85d42a942ec7f61e54db3
Maximilian Sinnl <max.sinnl@hotmail.com>
2023-06-06 18:28:27 +0200

    BB-239 sorted menu links like in slidemenu

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,9 +8,9 @@ function Home(props)
         <>
         <ContentWrapper>
             <LinkCard title="Profile" href="profile" icon="Profile"/>
+            <LinkCard title="Balance" href="balance" icon="Balance"/>
             <LinkCard title="Transactions" href="transactions" icon="Transaction"/>
             <LinkCard title="Goals" href="goals" icon="Goals"/>
-            <LinkCard title="Balance" href="balance" icon="Balance"/>
             <LinkCard title="Savings Settings" href="savings" icon="Savings"/>
         </ContentWrapper>
         </>
